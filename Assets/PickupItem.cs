589d7186b38be970ee056ac60dae7d2a42d60b63
ShaneKavage <42551406+ShaneKavage@users.noreply.github.com>
2022-01-22 17:08:04 -0600

    text mesh working

--- Assets/PickupItem.cs
+++ Assets/PickupItem.cs
@@ -7,13 +7,14 @@ public class PickupItem : MonoBehaviour
 {
     // Start is called before the first frame update
 
-    public float pickupRange = 360;
+    public float pickupRange = 2.5f;
     public Transform player;
     public GameObject UIObject;
     public HUD hud;
 
     public void Start()
     {
+
         hud = GameObject.FindGameObjectWithTag("UI").GetComponent<HUD>();
     }
 
@@ -21,6 +22,15 @@ public class PickupItem : MonoBehaviour
     {
         //Check if player is within range
         Vector3 distanceToPlayer = player.position - transform.position;
+        if (distanceToPlayer.magnitude > pickupRange)
+        {
+            UIObject.SetActive(false); 
+            return; 
+        }
+        if (distanceToPlayer.magnitude <= pickupRange) 
+        { 
+            UIObject.SetActive(true); 
+        }
         if (distanceToPlayer.magnitude <= pickupRange && Input.GetKeyDown(KeyCode.E)) { pickup(); }
     }
 
@@ -31,20 +41,5 @@ public class PickupItem : MonoBehaviour
        print("Got Object; Object Destoryed");
        Destroy(gameObject);
 
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.tag == "Player")
-        {
-            UIObject.SetActive(true);
-        }
-    }
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.tag == "Player")
-        {
-            UIObject.SetActive(false);
-        }
     }
 }
