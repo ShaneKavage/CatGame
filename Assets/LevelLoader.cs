f7ab0ab1a330ad0dfbf891f20c2af55b571e6e6f
ShaneKavage <42551406+ShaneKavage@users.noreply.github.com>
2022-01-23 11:32:21 -0600

    Audio in along with main menu.

--- Assets/LevelLoader.cs
+++ Assets/LevelLoader.cs
@@ -7,6 +7,7 @@ public class LevelLoader : MonoBehaviour
 {
 
     public Animator transition;
+    public string _Level;
     private float wait = 3f;
 
     // Update is called once per frame
@@ -15,17 +16,62 @@ public class LevelLoader : MonoBehaviour
         
     }
 
-    public void LoadCreditsScene()
+    public void LoadGameScene()
     {
         StartCoroutine(LoadLevel());
     }
+    public void LoadCreditsScene()
+    {
+        StartCoroutine(LoadCredits());
+    }
+    public void LoadGameMenu()
+    {
+        StartCoroutine(LoadMenu());
+    }
+    public void LoadInstructions()
+    {
+        StartCoroutine(Loadinstructions());
+    }
+    public void StopMusic()
+    {
+        SC_SCE_MusicManager obj = GameObject.FindGameObjectWithTag("MainScreenMusic").GetComponent<SC_SCE_MusicManager>();
+        obj.DestroyME();
+    }
+    public void PlayButtonNoise()
+    {
+        GetComponent<AudioSource>().Play();
+    }
 
+    IEnumerator LoadMenu()
+    {
+        transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(wait);
+
+        SceneManager.LoadScene("Menu");
+    }
     IEnumerator LoadLevel()
     {
         transition.SetTrigger("Start");
 
         yield return new WaitForSeconds(wait);
 
+        SceneManager.LoadScene(_Level);
+    }
+    IEnumerator LoadCredits()
+    {
+        transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(wait);
+
         SceneManager.LoadScene("Credits");
+    }
+    IEnumerator Loadinstructions()
+    {
+        transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(wait);
+
+        SceneManager.LoadScene("Instructions");
     }
 }
